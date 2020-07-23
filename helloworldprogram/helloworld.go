3b01d87655be876cebcafb3563e554540385c8b2
srikarthik123 <srikarthik.kante@gmail.com>
2020-07-23 14:39:27 +0530

    changed hello world program

--- helloworldprogram/helloworld.go
+++ helloworldprogram/helloworld.go
@@ -5,6 +5,20 @@ import (
 )
 
 func main() {
+	fmt.Println("i am just inserted")
+	foo()
+	for i := 0; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
 
-	fmt.Println("Hello world")
+	}
+	lol()
+}
+
+func foo() {
+	fmt.Println("i am before for loop")
+}
+func lol() {
+	fmt.Println("i am just came out of the loop")
 }
